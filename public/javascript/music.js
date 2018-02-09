bb03e56d5fdb6958ca5706761f712c86f8056add
Anurag Gupta <anuraggupta@gmail.com>
2018-02-10 03:41:04 +0530

    Uploading

---
+++ public/javascript/music.js
@@ -0,0 +1,21 @@
+window.addEventListener('keydown', function(el){
+
+const audio = document.querySelector(`audio[data-key="${el.keyCode}"]`);
+const key = document.querySelector(`.key[data-key="${el.keyCode}"]`);
+if(!audio)
+return;
+audio.currentTime=0;
+audio.play();
+key.classList.add('playing');
+
+
+}) 
+
+function removeTransition(el){
+	if(el.propertyName !== 'transform') return;
+	this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener("transitionend", removeTransition) );
+
